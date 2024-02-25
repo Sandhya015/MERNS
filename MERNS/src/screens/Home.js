9fe9b3a85a1e1f0e4494d1e160a555d2a46f7423
Sandhya <144054124+Sandhya015@users.noreply.github.com>
2024-02-25 23:21:35 +0530

    Add files via upload

---
+++ MERNS/src/screens/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function 
+
+() {
+  return (
+    <div>
+
+    <div>
+        <Navbar></Navbar>
+        </div>
+        <div>Body</div>   
+        <div><Footer></Footer></div> 
+    </div>
+  )
+}
